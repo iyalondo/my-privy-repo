73a4bcdf705432f09fdf7dec8bd36e22549dc5ee
oreofe ajose <oreofeajose@gmail.com>
2023-01-18 14:33:17 -0500

    updates

--- deploy.py
+++ deploy.py
@@ -1,3 +1,4 @@
 # Deploying a python web application
-echo "Hello World"   = shell scripts    
-python ('Hello World') = python script 
+#echo "Hello World"   = shell scripts    
+#python ('Hello World') = python script
+print ('Hello World')
